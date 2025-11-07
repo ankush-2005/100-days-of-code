ce8d1ae03b73380739589dfa77bf63299b0cd2e1
ankush kumar <ankushkumaryadav512@gmail.com>
2025-11-07 12:53:40 +0530

    Create Q32.c

---
+++ DAY 11-20/Q32.c
@@ -0,0 +1,18 @@
+/*Q32:Write a program to check if a number is a palindrome.
+  */
+#include <stdio.h>
+
+int main() {
+    int n, original, rev = 0;
+    scanf("%d", &n);
+    original = n;
+    while (n != 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    if (original == rev)
+        printf("Palindrome\n");
+    else
+        printf("Not palindrome\n");
+    return 0;
+}
